de953d990b4ae9e02ca1b29664e8ea7754960fc1
Haseebullah9012 <haseebullah.amanat@gmail.com>
2021-06-29 10:26:47 +0500

    Code Update (Configuration Options & Other Features) The Feature of Configuring the Game.  The Total Numbers of Cards to be Picked, can be Customized by the User.  The Feature of Choosing, whether the User wants to See the Swapping process or Not (Guess Blindly).  The Feature of Choosing How many Times to Swap.
    The Feature of Multiple Guesses (Based on the No. of Cards Generated).
    The Feature of Showing the Original Number of the Card, in case the User Lose.
    
    The ReadMe File is Also Updated Accordingly.

--- README.md
+++ README.md
@@ -1,33 +1,47 @@
 # Welcome to the CardGuess-Game
 
-Its a Simple Card Guessing Game. (Version 1)
+Its a Simple Card Guessing Game. (Version 1.2)
 
 ## How to Play:
 
-  1. At first, You would be seen Three Cards, randomly picked from a Deck. Like this, 
+  1. First, If You want, Do Some **_Configurations_**, 
+
+    - Enter the Custom Total **Number of Cards** to Picked from the Deck. 
+        - You can choose to Generate upto 26 Cards. 
+
+    - Choose, if you want to See the Swapping Process, or just wanted to Guess your Card Blindly. 
+    
+    - Also You can Enter How Many Times the Cards would be Swapped. 
+        - You can choose to Swap upto 52 Times (Difficulty). 
+
+
+  2. When you are Done Configuring, The Cards would be randomly picked from a Deck. Like this, 
 
       * Card 1: The Six of Spades. 
       * Card 2: The Three of Diamonds. 
       * Card 3: The Queen of Hearts. 
 
-  2. Put the Bet on any Card of your Choice, by just entering that Card-Number. 
+  3. Put the Bet on any Card of your Choice, by just entering that Card-Number. 
   
-  3. After that, the Cards would begin to Swap. Just wait, this would just take a Second. 
+  4. After that, the Cards would begin to Swap. Just wait, this would just take a Second. 
      
-  4. When it is Done Swapping, Guess where's that Card in which you put the Bet, 
-     by entering the Card-Number. 
+  5. When it is Done Swapping, Guess where's that Card, in which you put the Bet, by entering the Card-Number. 
 
-  5. If you Guess it correctly, You Win. Otherwise, you'll Lose. 
+  6. If you Guess it correctly, You Win. 
+     Otherwise, Guess Again, if you have any left. 
 
-  6. If you Enjoyed! You can Play it Again. 
+  7. If you Ran Out of Guesses, You'll Lose. 
+
+  8. If you Enjoyed! You can Play it Again. 
 
 
 #### Note:
 
-  * The Probability of picking the Three Cards is Equal. 
+  * The Probability of Picking the Cards is Equal (There's no Inequality). 
   * The Swapping is also Done Randomly with equal amount of Probability. 
   
-  * If you are Lucky, Two Same Cards could show up, or even all the Three Cards could be Same 
+  * If you are Lucky, Two Same Cards could show up, or even all the Picked Cards could be Same 
     (but its Probability is very very Low, which surely would mean that You are One of the Luckiest Person). 
-  * Its also possible that the Card just swapped by itself. But don't Worry, they are Swapped Multiple Times. 
+  * Its also possible that the Card just swapped by itself. But don't Worry, they are Swapped Multiple Times, so this won't Happen each Time. 
 
+  * If you'll Lose, your Exact Bet Card Number would be Shown, just in Case, if you dont believe us, or you just desired to know the exact Number. 
--- game.cpp
+++ game.cpp
@@ -65,31 +65,40 @@ struct Card
     };
 };
 
+void configuration();
+void Cards(); //To choose the Total Number of Cards
+void SwapProcess(); //To choose to Enable/Disable the Swap Process Option
+
 void GenerateCards();
 void chooseCard(Card&); //For Choosing the Card
 void Swap(Card, Card); //For simply Swapping the Cards
 void evaluate();
 void PlayAgain(); //The PlayAgain Option
 
-Card card[3]; //The Cards
+int cards = 3; //The Total Default Number of Cards
+Card card[26]; //The Cards
 Card bet, guess; //The User Bet-Card & the chosen Guess-Card
 int c; //For Loop-control Cards
 
+char configure = 'N'; //The Default Configuration Options 
+char swapProcess = 'N'; //The Default Guessing Option
+int swap_times = 9; //The Default Times the Cards would be Swapped
+int guesses; //The Times the User had Guesses
 int max_cIgnore = 255; //MaxCharacters to be Ignored in the Buffer
 char playAgain = 'Y'; //The Default PlayAgain Option
 
 random_device Seed; //RandomValue Generator
-uniform_int_distribution<int> rand_card(0, 2); //For Randomly Generating the Card
-uniform_int_distribution<int> rand_number(2, 14); //For Randomly Generating the CardNumber
-uniform_int_distribution<int> rand_suit(1, 4); //For Randomly Generating the CardSuit
 
 int main()
 { 
     cout << endl
 		<< "Welcome to the Sinlge-Player Card Guessing Game. \n"
 		<< "You are Advised to Consult the Readme File, before Playing the Game. \n"
-		<< endl;
-
+        << endl << endl;
+	
+    int a,b;	
+    configuration();
+	    
     do {
         cout << "Lets Begin! \n\n";
         
@@ -99,14 +108,24 @@ int main()
         chooseCard(bet);
         
         cout << "let me Swap the Cards! \n";
-        for(c=0; c<3*2; c++)
-            Swap(card[rand_card(Seed)], card[rand_card(Seed)]);
-        sleep(1);
+        uniform_int_distribution<int> rand_card(0, cards-1); //For Randomly Generating the Card
+        for(c=0; c<swap_times; c++) {
+            a = rand_card(Seed), b = rand_card(Seed);
+            Swap(card[a], card[b]);
+            
+            if(swapProcess == 'Y')
+                cout << "  Swapped the Card " << a+1 << " with Card " << b+1 << ". \n";
+        }
+        if(!(swapProcess == 'Y'))
+            sleep(1);
+        cout << endl;
         
-        cout << "Ok Now Guess where The " << bet.numberName << " of " << bet.suitName << " (Your Bet-Card) is: ";
-        chooseCard(guess);
 
-        evaluate();
+        cout << "Ok Now Guess where The " << bet.numberName << " of " << bet.suitName << " (Your Bet-Card) is: ";
+        while(true) {
+            chooseCard(guess);
+            evaluate();
+        }
 
         PlayAgain();
     }
@@ -116,10 +135,103 @@ int main()
     return 0;
 }
 
+void configuration()
+{
+    do {	
+		if(configure == 'Y') {
+			Cards();
+            SwapProcess();
+		}
+		
+		cout << "The Total of " << cards << " Cards would be Picked randomly from the Deck. \n";
+
+        if(swapProcess == 'Y')
+		    cout << "You would be Shown the Swapping Process of the Cards. \n";
+        else
+            cout << "You will Guess Your Card Blindly, without Seeing the Swapping Process. \n";
+		
+        cout << "The Cards would be Swapped " << swap_times << " Times. \n";
+        cout << "And You have " << (cards/3) << " Chance to Guess Correctly. \n";
+        cout << "   Do you want to Configure it (Y/N): ";
+		
+		cin >> configure;
+		configure = toupper(configure);
+		cin.ignore(max_cIgnore, '\n');
+		
+		cout << endl << endl;
+	}
+	while(configure == 'Y');
+}
+
+void Cards()
+{
+    cout << "Enter the Custom Total Number of Cards to be Picked: ";
+    cin >> cards;
+    cin.ignore(max_cIgnore,'\n');
+    
+    if (cin.fail()) {
+        cin.clear(); cin.ignore(max_cIgnore,'\n');
+        cout << "   Oops! Its not the legal Number of Cards. \n\n";
+        cout << "Again, ";
+        Cards();
+    }
+    else if (!(cards>=2)) {
+        cout << "   Oops! You Choose to Pick very Few Cards to Play. \n\n";
+        cout << "Again, ";
+        Cards();
+    }
+    else if (!(cards<26)) {
+        cout << "   Oops! You Choose to Pick a large Amount of Cards to Play. \n\n";
+        cout << "Again, ";
+        Cards();
+    }
+    cout << endl; //It would be Buffered each Time the Function Self-Calls
+}
+
+void SwapProcess()
+{
+    cout << "Do You Want to See the Swapping Process (Y/N): ";
+    cin >> swapProcess;
+    swapProcess = toupper(swapProcess);
+    cin.ignore(max_cIgnore,'\n');
+    
+    if(swapProcess == 'Y') {
+        cout << "Enter How Many Times To Swap: ";
+        cin >> swap_times;
+        cin.ignore(max_cIgnore,'\n');
+    
+        if (cin.fail()) {
+            cin.clear(); cin.ignore(max_cIgnore,'\n');
+            cout << "   Oops! Its not the a Number. \n\n";
+            cout << "Again, ";
+            SwapProcess();
+        }
+        else if (!(swap_times>=3)) {
+            cout << "   Oops! You Choose to Swap very few Times. \n\n";
+            cout << "Again, ";
+            SwapProcess();
+        }
+        else if (!(swap_times<52)) {
+            cout << "   Oops! You Choose to to Swap very much More Times. \n\n";
+            cout << "Again, ";
+            SwapProcess();
+        }
+    }
+    else {
+        cout << "This option is Set to Deafult. \n";
+        swapProcess = 'N';
+        swap_times = cards * 3;
+    }
+    cout << endl; //It would be Buffered each Time the Function Self-Calls
+}
+
 void GenerateCards()
 {
     cout << "Here are the Cards: \n";
-    for(c=0; c<3; c++) {
+    uniform_int_distribution<int> rand_number(2, 14); //For Randomly Generating the CardNumber
+    uniform_int_distribution<int> rand_suit(1, 4); //For Randomly Generating the CardSuit
+
+    for(c=0; c<cards; c++) {
         card[c].number = rand_number(Seed); 
         card[c].suit = rand_suit(Seed);
         
@@ -143,17 +255,15 @@ void chooseCard(Card &n)
         cout << "Choose the Card Again: ";
         chooseCard(n);
     }
-    else if(!(choose>=1 && choose<=3)) {
+    else if(!(choose>=1 && choose<=cards)) {
         cout << "   Oops! There's no such Card. \n";
         cout << "Choose the Card Again: ";
         chooseCard(n);
     }
     
-    for(c=0; c<3; c++)
+    for(c=0; c<cards; c++)
         if(choose == c+1)
             n = card[c];
-    
-    cout << endl; //It would be Buffered each Time the Function Self-Calls
 }
 
 void Swap(Card a, Card b)
@@ -165,14 +275,21 @@ void Swap(Card a, Card b)
 
 void evaluate()
 {
-    if(guess.number == bet.number && guess.suit == bet.suit)
-        cout << "Great! You Won! \n\n";
-    else {
-        cout << "Ahh! You Lose! \n";
-        for(c=0; c<3; c++)
-            if(bet.number == card[c].number && bet.suit == card[c].suit)
-                cout << "It was on Card no." << c+1 << ". \n\n";
+    if(guess.number == bet.number && guess.suit == bet.suit) {
+        cout << "\n\nGreat! You Won! \n";
     }
+    else if(guesses<(cards/3)) {
+        cout << "Nah! Guess Again: ";
+        guesses++;
+        return;
+    }
+    else {
+        cout << "\n\nAhh! You Ran Out of Guessses. You Lose! \n";
+        for(c=0; c<cards; c++)
+            if(bet.number == card[c].number && bet.suit == card[c].suit)
+                cout << "It was on Card no." << c+1 << ". \n";
+    }
+    cout << endl;
 }
 
 void PlayAgain()
@@ -183,8 +300,14 @@ void PlayAgain()
 	cin.ignore(max_cIgnore, '\n');
 
 	if(playAgain == 'Y') {
+		cout << "Do You Want to Configure the Options (Y/N): ";	
+		cin >> configure;
+		configure = toupper(configure);
+		cin.ignore(max_cIgnore, '\n');
+		
 		cout << endl << endl;
-        return;
+		if(configure == 'Y')
+			configuration();
 	}
 	else if(playAgain == 'N') {
 		cout << endl << endl;
